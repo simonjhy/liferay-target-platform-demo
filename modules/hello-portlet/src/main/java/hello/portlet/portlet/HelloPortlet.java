add47e41c4abbd84cb4691c335c995cb478167dc
Raymond Augé <raymond.auge@liferay.com>
2019-05-14 22:56:51 -0400

    fleshed out with more modules

--- modules/hello-portlet/src/main/java/hello/portlet/portlet/HelloPortlet.java
+++ modules/hello-portlet/src/main/java/hello/portlet/portlet/HelloPortlet.java
@@ -2,11 +2,18 @@ package hello.portlet.portlet;
 
 import hello.portlet.constants.HelloPortletKeys;
 
+import com.foo.api.MyApi;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 
+import java.io.IOException;
+
 import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author greg
@@ -25,4 +32,17 @@ import org.osgi.service.component.annotations.Component;
 	service = Portlet.class
 )
 public class HelloPortlet extends MVCPortlet {
+
+	@Override
+	protected void doDispatch(RenderRequest renderRequest, RenderResponse renderResponse)
+			throws IOException, PortletException {
+
+		renderRequest.setAttribute("json", myApi.convertNameToJSON("some text to be jsonified"));
+
+		super.doDispatch(renderRequest, renderResponse);
+	}
+
+	@Reference
+	private MyApi myApi;
+
 }
